0fd9518d1db4799f7ed569f4d090a2774ac52ad0
Tunde Adepitan <tunde262@gmail.com>
2025-05-07 15:10:08 -0500

    Added Tanstack Query & 1st hook for example

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,24 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { useEffect } from 'react';
+import { createFileRoute } from '@tanstack/react-router';
+
+// Hooks (tanStack Query)
+import { useExample } from '../hooks/useExample';
 
 export const Route = createFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+
+    // Product Data
+  const { data, isLoading, isError } = useExample();
+
+  useEffect(() => {
+    if (data) {
+      console.log("EXAMPLE DATA FROM TANSTACK QUERY", data);
+    }
+  }, [data]);
+
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
