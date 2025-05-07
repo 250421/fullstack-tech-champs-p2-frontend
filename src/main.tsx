5d1434a637931f4a0f38ba9313d5f56290463cef
Tunde Adepitan <tunde262@gmail.com>
2025-05-07 15:18:07 -0500

    Added Tailwind CSS

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
+
+// Tanstack stuff (Router & Query)
 import { RouterProvider, createRouter } from '@tanstack/react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
